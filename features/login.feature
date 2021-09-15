cb4afff8059dee5eccacdd16422d2a516ab54836
Artur Sargsyan <artur_sargsyan@epam.com>
2021-09-15 12:06:30 +0400

    create page object

--- features/login.feature
+++ features/login.feature
@@ -5,8 +5,9 @@ Feature: The Internet Guinea Pig Website
     Given I am on the login page
     When I login with <username> and <password>
     Then I should see a flash message saying <message>
-
+    Then I should wait 5
     Examples:
       | username | password             | message                        |
       | tomsmith | SuperSecretPassword! | You logged into a secure area! |
       | foobar   | barfoo               | Your username is invalid!      |
+    
--- step-definitions/actions.ts
+++ step-definitions/actions.ts
@@ -1,15 +1,30 @@
-import LoginPage from '../pageobjects/login.page';
-import {Given, When} from "@cucumber/cucumber";
+import LoginPage from '../pageobjects/pages/login.page';
+import {BasePage} from '../pageobjects/pages/base.page';
+import {Given, Then, When} from "@cucumber/cucumber";
+import * as stepFunctions from "../step-definitions/utils/stepFunctions"
+const BasePageInstance = new BasePage();
 
 const pages = {
     login: LoginPage
 };
 
-Given(/^I am on the (\w+) page$/, async (page: string) => {
+Then(/^I should wait (.*)$/, async (milliseconds: number) => {
+    await browser.pause(milliseconds * 1000)
+})
+
+Given(/^I am on the (.*) page$/, async (page: string) => {
     await pages[page].open()
 });
 
-When(/^I login with (\w+) and (.+)$/, async (username: string, password: string) => {
+When(/^I login with (.*) and (.*)$/, async (username: string, password: string) => {
     await LoginPage.login(username, password)
 });
 
+When(/^(Eventually )?I type (.*) in (.*)$/, async (eventually:string, text: string, element: any) => {
+    const locator = await stepFunctions.findElement(element);
+    if(eventually) {
+        await BasePageInstance.waitElementBeClickable(locator)
+        await locator.setValue(text);
+    }
+    await locator.setValue(text);
+});
