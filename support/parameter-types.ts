2234a9b0e8da9c76ef063c1f894f1c870fc0e986
Ernest Yedigaryan <ernest_yedigaryan@epam.com>
2021-09-22 10:24:10 +0400

    Adding Parameter Types Implementation

---
+++ support/parameter-types.ts
@@ -0,0 +1,21 @@
+import { defineParameterType } from '@cucumber/cucumber';
+
+/**
+ * Used for returning chai assertion condition
+ */
+defineParameterType({
+    regexp: /exist|be enabled|be displayed|be clickable|be focused|be displayed in viewport/,
+    transformer(condition) {
+        const CONDITIONS = {
+            exist: 'waitForExist',
+            'be enabled': 'waitForEnabled',
+            'be displayed': 'waitForDisplayed',
+            'be clickable': 'waitForClickable',
+            'be focused': 'waitForElementFocused',
+            'be displayed in viewport': 'waitForElementDisplayedInViewport',
+        };
+        return CONDITIONS[condition];
+    },
+    name: 'wait condition',
+    useForSnippets: false,
+});
